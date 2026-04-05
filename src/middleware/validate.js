03902e7b42eb4c9e7409b3a24eade5b0c0ce9060
Himanshu Jorwal <juventusstark1221@gmail.com>
2026-04-06 00:44:23 +0530

    Implement request validation middleware
    Add validation middleware to handle request validation errors.

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -1,1 +1,15 @@
+const { validationResult } = require('express-validator');
+const { sendError } = require('../utils/response');
 
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return sendError(res, 'Validation failed', 422, errors.array().map(e => ({
+      field: e.path,
+      message: e.msg,
+    })));
+  }
+  next();
+};
+
+module.exports = { validate };
